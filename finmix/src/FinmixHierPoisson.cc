e3e13da19a2149dcc55c15443c07dc7d5faf6a58
simon.zehnder <simon.zehnder@googlemail.com>
2013-07-24 17:18:53 +0200

    Deleting unused files from repository.

--- finmix/src/FinmixHierPoisson.cc
+++
@@ -1,9 +0,0 @@
-#include <RcppArmadillo.h>
-#include "FinmixHierPoisson.h"
-
-FinmixHierPoisson::FinmixHierPoisson (const Rcpp::List& hyper)
-{
-	Rcpp::NumericVector tmpB(hyper["b"]);
-	unsigned int M = tmpB.nrows();
-	hyper(tmpB.begin(), M, false, true);
-}
